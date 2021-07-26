40a793785f7cd6de525030da4f7338861a972401
1996-isuru <isurusrimalnt@gmail.com>
2021-07-26 13:22:05 +0530

    complete add student form and routing

--- web-application/src/App.js
+++ web-application/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import Header from "./components/Header";
+import AddStudent from "./components/AddStudent";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/add" exact component={AddStudent} />
+      </div>
+    </Router>
   );
 }
 
